30eeec8d17f86ad8e10615735e3e84f836b5725b
Rick429 <richaristico@gmail.com>
2022-01-27 12:46:43 +0100

    UserServiceImpl AddUser Test

--- src/test/java/com/sopromadze/blogapi/service/impl/TagServiceImplTest.java
+++ src/test/java/com/sopromadze/blogapi/service/impl/TagServiceImplTest.java
@@ -1,4 +1,4 @@
-package com.sopromadze.blogapi.service;
+package com.sopromadze.blogapi.service.impl;
 
 import com.sopromadze.blogapi.exception.ResourceNotFoundException;
 import com.sopromadze.blogapi.model.Tag;
